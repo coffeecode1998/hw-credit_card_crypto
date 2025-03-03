3b727de8c2e1a558a634c5971b50c04955e2ee4f
carrie0925 <ca0983896371@gmail.com>
2025-03-03 14:14:56 +0800

    assignment update

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -7,5 +7,17 @@ module LuhnValidator
     nums_a = number.to_s.chars.map(&:to_i)
 
     # TODO: use the integers in nums_a to validate its last check digit
+    sum = 0
+    nums_a[0...-1].reverse.each_with_index do |digit, i|
+      if i.even?
+        doubled = digit * 2
+        doubled = doubled > 9 ? doubled - 9 : doubled
+        sum += doubled
+      else
+        sum += digit
+      end
+    end
+    check_digit = nums_a.last
+    (sum + check_digit) % 10 == 0
   end
 end
